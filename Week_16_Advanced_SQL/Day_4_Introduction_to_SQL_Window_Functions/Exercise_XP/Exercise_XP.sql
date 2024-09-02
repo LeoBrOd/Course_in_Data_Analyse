c7d8e6e6153c45c4cbe7e8522618c741e128dcf6
LeoBrOd <brodskiy.l.v@gmail.com>
2024-09-02 21:25:28 +0300

    Update

---
+++ Week_16_Advanced_SQL/Day_4_Introduction_to_SQL_Window_Functions/Exercise_XP/Exercise_XP.sql
@@ -0,0 +1,5 @@
+-- Exercise 1: Movie Rankings and Analysis
+
+-- Task 1: Rank Movies by Popularity within Each Genre
+-- Use the RANK() function to rank movies by their popularity within each genre. Display the genre name, movie title, and their rank based on popularity.
+
